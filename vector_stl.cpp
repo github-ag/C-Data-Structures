a9756fe726913c9bc67f8a455699cbc4f7c592da
Abhishek Garg <24725199+github-ag@users.noreply.github.com>
2019-03-11 18:54:30 +0530

    Create vector_stl.cpp

---
+++ vector_stl.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+    vector<int> v;
+    v.reserve(100);//makes its capacity initially to 100
+    // it is better to initially reserve some capacity as exapnding array is expensive function
+    for(int i=0;i<=5;i++)
+        v.push_back(i*i);
+    for(int j=0;j<v.size();j++)
+    {
+        cout<<v.at(j)<<" ";
+    }
+    vector<int> v2(3,100);
+    cout<<"\n";
+    for(int k=0;k<v2.size();k++)
+        cout<<v2[k]<<" ";
+
+    //swap function
+    swap(v,v2);
+    cout<<"\nvalues after swapping are->\n";
+      for(int j=0;j<v.size();j++)
+    {
+        cout<<v.at(j)<<" ";
+    }
+    cout<<"\n";
+    for(int k=0;k<v2.size();k++)
+        cout<<v2[k]<<" ";
+
+
+    cout<<"\nmax size->"<<v.max_size();//depends upom the RAM
+    cout<<"\ncapacity ->"<<v.capacity();
+    return 0;
+}
